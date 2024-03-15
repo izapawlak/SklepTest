7d1bd1941dedcca381800ddeb7b1729db0e64b67
Izabela Pawlak <izabela.malgorzata.pawlak@gmail.com>
2024-03-15 12:18:49 +0100

    switch in SetupTests

--- src/test/java/utils/SetupTests.java
+++ src/test/java/utils/SetupTests.java
@@ -3,6 +3,8 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import pages.MainPage;
@@ -14,15 +16,37 @@ public class SetupTests {
     private String appUrl = "https://skleptest.pl/";
 
     @BeforeSuite
-    protected void setupTests(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    protected void setupTests() {
+
+        String browser = reader.getBrowser();
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                driver.manage().window().maximize();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+                break;
+        }
     }
+
 
     protected MainPage openMainPage(){
         driver.get(reader.getAppUrl());
-        //napisać asercję
         return new MainPage(driver);
     }
 
